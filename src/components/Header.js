55eb85a1fe19c34b79f333e0e16c4a07cc990ac3
Roshni Jha <roshnijha21@navgurukul.org>
2023-10-17 12:09:27 +0530

    header grid

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+const Header=()=>{
+    return(
+        <>
+            <div className="header">
+                <div className="head_cont"> 
+                    <div className="logo">logo</div>
+                    <div className="info">
+                        <div className="cate"></div>
+                        <div className="trend"></div>
+                        <div className="combo"></div>
+                        <div className="blog"></div>
+                        <div className="help"></div>
+                    </div>
+                    <div className="personal">
+                        <div className="car"></div>
+                        <div className="person"></div>
+                        <div className="search"></div>
+                        <div className="bag"></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+};
+export default Header;
